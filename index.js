db1f075a7a8a4b4d4913db93d003720c7875c88b
Tzzent <cr9474320@gmail.com>
2023-02-20 09:28:05 -0500

    Uploading summary files

---
+++ index.js
@@ -0,0 +1,28 @@
+// const fetchData = fetch('./data.json').then(res => res.json).then()
+
+const fetchData = async () => {
+  const result = await fetch('./data.json');
+  const data = await result.json();
+  return data;
+};
+
+const main = async () => {
+  const summaryContainer = document.querySelector('.cards');
+  const data = await fetchData();
+
+  data.map((d, idx) => {
+    summaryContainer.innerHTML += `
+       <div class='card ${d.category}'>
+          <div class='category'>
+            <img src='${d.icon}'/>
+            <p class='title${idx + 1}'>${d.category}</p>
+          </div>
+          <div class='points'>
+            <p><b>${d.score}</b> / 100</p>
+          </div>
+       </div>
+       `;
+  })
+}
+
+main();
